be4aa0da5ba34c4d5236733758185c1f946c0d13
Marco Vargas <mav89ls@gmail.com>
2019-04-20 10:09:06 -0700

    Finished writing my own closure in closure.js file, made it about the solar system.

--- assignments/closure.js
+++ assignments/closure.js
@@ -2,12 +2,35 @@
 // Write a simple closure of your own creation.  Keep it simple!
 
 
+function solarsystem() {
+    const star = "the Sun";
+    console.log(`I am the solar system, my biggest celestial object is ${star}.`);
+
+    function earth() {
+        const planet = "the Earth"
+        console.log(`I am planet earth, I rotate around ${star}.`);
+
+        function moon() {
+            console.log(`I am the moon, I rotate around ${planet}. `);
+        } //moon scope
+        moon();
+
+    } //earth scope
+    earth();
+
+} //solar scope
+solarsystem()
+
+
+
+
+
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+    // Return a function that when invoked increments and returns a counter variable.
 };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
@@ -15,7 +38,7 @@ const counter = () => {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+    // Return an object that has two methods called `increment` and `decrement`.
+    // `increment` should increment a counter variable in closure scope and return it.
+    // `decrement` should decrement the counter variable and return it.
 };
